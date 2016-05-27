ff9300a917c158100f04669837b23cd7f4df179f
Nathan West <nathanw@dropbox.com>
2016-05-27 18:01:49 -0400

    Added production webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack')
+module.exports = {
+	entry: './src/main.jsx',
+	output: {
+		path: 'dist.prod',
+		filename: 'bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Beach Week Karaoke Song Finder",
+		}),
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery"
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': '"production"'
+		}),
+	],
+	module: {
+		loaders: [{
+			test: /\.json$/,
+			loader: 'json'
+		}, {
+			test: /\.jsx$/,
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015', 'react']
+			}
+		},
+
+		{ test: /\.css$/, loader: "style-loader!css-loader" },
+
+		// Bootstrap loaders
+		{test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+		{test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+		{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+		{test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}]
+	}
+}
