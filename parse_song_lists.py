4b8352c2b89930c08e6f58387128e0c51727093f
Nathan West <nathanw@dropbox.com>
2016-05-27 18:00:00 -0400

    Updates to parse_song_list.py

--- parse_song_lists.py
+++ parse_song_lists.py
@@ -1,18 +1,20 @@
 import plistlib
-import logging
 import re
 import sys
 import json
+from collections import namedtuple
+from functools import wraps
 from os import path
 
 from autocommand import autocommand
 
 
+def message(*args, **kwargs):
+    print(*args, file=sys.stderr, flush=True, **kwargs)
+
 artist_title_matchers = [
     re.compile(r"^.* - (?P<artist>.*) - (?P<title>.*)$"),
-    re.compile(r"^(?P<artist>.*) - (?P<title>.*) \[.*\]$", re.I),
     re.compile(r"^(?P<artist>.*) - (?P<title>.*)$"),
-    re.compile(r"^(?P<artist>.*)-(?P<title>.*) \[.*\]$", re.I),
     re.compile(r"^(?P<artist>.*)-(?P<title>.*)$"),
 ]
 
@@ -21,16 +23,16 @@ bad_titles = [
     re.compile(r"^Track [0-9]+", re.I)
 ]
 
+
 def load_music_file(filename):
     with open(filename, 'rb') as file:
         return plistlib.load(file)
 
 
-class ParseError(Exception):
-    pass
+Song = namedtuple('Song', 'title artist id disabled source')
 
 
-seen_ids = {}
+class ParseError(Exception): pass
 
 
 def check_bad_title(title, artist):
@@ -45,60 +47,58 @@ def check_bad_title(title, artist):
 
     return title, artist
 
-def extract_songs(data, source, need_strm):
-    source = path.basename(source)
-    playlists = data['Playlists']
-    for playlist in playlists:
-        items = playlist['Playlist Items']
-        for item in items:
-            song_id = item['soID']
-            if song_id in seen_ids:
-                logging.error(
-                    "Duplicate song id '%s' (Current title: %s, Original title: %s)",
-                    song_id, item.get('name', ""), seen_ids[song_id])
-                continue
-            else:
-                seen_ids[song_id] = item.get('name', "")
 
-            try:
-                title = item['name']
-            except KeyError:
-                logging.error("Song with id %s has no title", song_id)
-                continue
+def extract_song(item, need_strm, source):
+    song_id = item['soID']
+    try:
+        title = item['name']
+    except KeyError:
+        message("Song with id {} has no title".format(song_id))
+        raise ParseError()
 
-            try:
-                artist = item['arts']
-            except KeyError:
-                # Try to get the artist from the combined name in the title
-                for matcher in artist_title_matchers:
-                    match = matcher.match(title)
-                    if match:
-                        title = match.group('title')
-                        artist = match.group('artist')
-                        break
-                else:
-                    artist = ""
+    try:
+        artist = item['arts']
+    except KeyError:
+        # Try to get the artist from the combined name in the title
+        for matcher in artist_title_matchers:
+            match = matcher.match(title)
+            if match:
+                title = match.group('title')
+                artist = match.group('artist')
+                break
+        else:
+            artist = ""
 
-            title, artist = check_bad_title(title, artist)
+    title, artist = check_bad_title(title, artist)
 
-            if need_strm:
-                strm = item.get('strm', 0)
-                disabled = not bool(strm)
-            else:
-                disabled = False
+    if need_strm:
+        strm = item.get('strm', 0)
+        disabled = not bool(strm)
+    else:
+        disabled = False
 
-            yield {
-                'title': title,
-                'artist': artist,
-                'id': song_id,
-                'disabled': disabled,
-                'source': source
-            }
+    return Song(
+        title=title,
+        artist=artist,
+        id=song_id,
+        disabled=disabled,
+        source=source)
 
 
 def extract_file(filename, special):
     data = load_music_file(filename)
-    yield from extract_songs(data, filename, special)
+    source = path.basename(filename)
+    playlists = data['Playlists']
+
+    for playlist in playlists:
+        items = playlist['Playlist Items']
+        for item in items:
+            try:
+                song = extract_song(item, special, source)
+            except ParseError:
+                pass
+            else:
+                yield song
 
 
 def extract_all(special_file, files):
@@ -109,7 +109,27 @@ def extract_all(special_file, files):
         yield from extract_file(normal_file, False)
 
 
+def filter_extracted_songs(songs, dedup):
+    if dedup:
+        key = lambda song: (song.title, song.artist, song.source)
+    else:
+        key = lambda song: song.id
+
+    seen = {}
+
+    for song in songs:
+        k = key(song)
+        if k not in seen:
+            yield song
+            seen[k] = song
+        else:
+            message("Ignored duplicate song\n\tOriginal: {}\n\tDuplicate:{}".format(seen[k], song))
+
+
 @autocommand(__name__)
-def main(special_file=None, *files):
-    logging.basicConfig(level=logging.INFO)
-    json.dump(list(extract_all(special_file, files)), sys.stdout)
+def main(special_file=None, deduplicate=False, *files):
+    songs = extract_all(special_file, files)
+    songs = filter_extracted_songs(songs, deduplicate)
+    songs = [song._asdict() for song in songs]
+    json.dump(songs, sys.stdout)
+    message("Song list parsed: found {} songs".format(len(songs)))
